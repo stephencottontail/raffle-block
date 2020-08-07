276f90929c60fcd833813d6554d98073b8524a30
Stephen Dickinson <stephencottontail@me.com>
2020-08-07 13:50:56 -0600

    Set up 'developer mode'
    Developer mode means our stylesheet won't be loaded so a developer can
    more easily match their existing site

--- raffle-block.php
+++ raffle-block.php
@@ -60,11 +60,64 @@ class SC_Raffle extends WP_Block_Type {
 
 add_action( 'init', function() {
 	$assets = require( 'dist/index.asset.php' );
+
 	wp_register_script( 'sc-raffle-editor-script', plugins_url( 'dist/index.js', __FILE__ ), $assets['dependencies'], $assets['version'], true );
-	wp_register_style( 'sc-raffle-style', plugins_url( 'dist/style.css', __FILE__ ) );
+	if ( empty( get_option( 'raffle_block_options' ) ) ) {
+		wp_register_style( 'sc-raffle-style', plugins_url( 'dist/style.css', __FILE__ ) );
+		wp_enqueue_style( 'sc-fonts', 'https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600&display=swap' );
+	}
 
 	$raffle = new SC_Raffle();
 	register_block_type( $raffle );
 
-	wp_enqueue_style( 'sc-fonts', 'https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600&display=swap' );
 } );
+
+add_action( 'admin_menu', function() {
+	add_options_page( 'Raffle Block', 'Raffle Block', 'manage_options', 'raffle-block', 'raffle_block_callback' );
+} );
+
+add_action( 'admin_init', function() {
+	if ( false == get_option( 'raffle_block_options' ) ) {
+		add_option( 'raffle_block_options' );
+	}
+
+	register_setting( 'cottontail', 'raffle_block_options' );
+	add_settings_section( 'developer_mode', 'Developer Mode', 'raffle_block_developer_mode_instructions', 'raffle-block' );
+	add_settings_field( 'activate_developer_mode', 'Activate Developer Mode?', 'raffle_block_developer_mode_fields', 'raffle-block', 'developer_mode', array( 'label_for' => 'for_developer_mode' ) );
+} );
+
+function raffle_block_callback() {
+	ob_start();
+	settings_fields( 'cottontail' );
+	do_settings_sections( 'raffle-block' );
+	submit_button();
+	$settings = ob_get_clean();
+
+	$output = '<div class="wrap">';
+	$output .= sprintf( '<h1>%s</h1>', 'Raffle Block Options' );
+	$output .= '<form action="options.php" method="post">';
+	$output .= $settings;
+	$output .= '</form>';
+	$output .= '</div>';
+
+	echo $output;
+}
+
+function raffle_block_developer_mode_instructions() {
+	echo '<p>"Developer Mode" prevents the plugin&rsquo;s stylesheet from being loaded on the front-end.</p>';
+}
+
+function raffle_block_developer_mode_fields() {
+	$options = get_option( 'raffle_block_options' );
+	$checked = '';
+
+	/**
+	 * `$options` is empty if developer mode is not set. i think
+	 * this might be because there's only one option under that
+	 * key. something to be aware of.
+	 */
+	if ( ! empty( $options ) ) {
+		$checked = 'checked';
+	}
+	echo sprintf( '<input id="for_developer_mode" name="raffle_block_options[developer_mode]" type="checkbox" %s />', $checked );
+}
